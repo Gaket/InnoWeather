b71bd0953032e063f1efc17cff02c3beebfbe59e
Artur <artusletter@gmail.com>
2016-07-15 18:20:15 +0300

    Bugfix

--- app/src/main/java/ru/innopolis/innoweather/presentation/view/activity/MainActivity.java
+++ app/src/main/java/ru/innopolis/innoweather/presentation/view/activity/MainActivity.java
@@ -2,6 +2,8 @@ package ru.innopolis.innoweather.presentation.view.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.Toolbar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -10,10 +12,11 @@ import ru.innopolis.innoweather.presentation.di.HasComponent;
 import ru.innopolis.innoweather.presentation.di.components.DaggerUserComponent;
 import ru.innopolis.innoweather.presentation.di.components.UserComponent;
 import ru.innopolis.innoweather.presentation.di.modules.UserModule;
+import ru.innopolis.innoweather.presentation.model.CityModel;
 import ru.innopolis.innoweather.presentation.view.fragment.CitiesListFragment;
 import ru.innopolis.innoweather.presentation.view.fragment.WeatherDetailsFragment;
 
-public class MainActivity extends BaseActivity implements HasComponent<UserComponent> {
+public class MainActivity extends BaseActivity implements HasComponent<UserComponent>, CitiesListFragment.CityListListener {
 
     private static final String INTENT_EXTRA_PARAM_CITY_ID = "RU.INNO_WEATHER.INTENT_PARAM_CITY_ID";
 
@@ -35,7 +38,7 @@ public class MainActivity extends BaseActivity implements HasComponent<UserCompo
     private void initializeActivity(Bundle savedInstanceState) {
         if (savedInstanceState == null) {
             // TODO: remove default cityid
-            this.cityId = getIntent().getIntExtra(INTENT_EXTRA_PARAM_CITY_ID, 2172797);
+            this.cityId = getIntent().getIntExtra(INTENT_EXTRA_PARAM_CITY_ID, -1);
             addFragment(R.id.fragmentContainer, new CitiesListFragment());
         } else {
             // restore state
@@ -56,4 +59,11 @@ public class MainActivity extends BaseActivity implements HasComponent<UserCompo
         return userComponent;
     }
 
+    @Override
+    public void onCityClicked(CityModel cityModel) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragmentContainer, new WeatherDetailsFragment());
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
 }
